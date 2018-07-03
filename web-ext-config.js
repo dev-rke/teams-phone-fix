d653864e518bfdf4f83be90c6a540bad58dcb963
Reiner Kempkes <kempkes.reiner@gmail.com>
2018-07-03 02:08:40 +0200

    improved build environment

---
+++ web-ext-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ignoreFiles: [
+    'node_modules',
+    'web-ext-artifacts',
+    '.editorconfig',
+    '.eslintrc',
+    '.gitignore',
+    '.gitignore',
+    'package.json',
+    'package-lock.json',
+    'web-ext-config.js',
+  ],
+  build: {
+    overwriteDest: true,
+  },
+};
