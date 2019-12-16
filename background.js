dc7429ac12c4f65a7f61126de11e9d73c3037d15
Borys Kabakov <mailrupile@gmail.com>
2019-12-16 14:59:15 +0200

    Update background.js
    #1

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 const targetPage = 'https://teams.microsoft.com/*';
 
 //const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134';
-const userAgent = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134';
-
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
 function rewriteUserAgentHeader(e) {
   for (const header of e.requestHeaders) {
     if (header.name.toLowerCase() === 'user-agent') {
